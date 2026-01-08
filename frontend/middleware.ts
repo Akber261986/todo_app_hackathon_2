2c58ecf3ffaffea6aabb6b5e4386e097aa0a97e5
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2026-01-08 16:00:31 +0500

    regex instead of old

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -44,12 +44,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(png|jpg|jpeg|gif|svg|ico|webp)$).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 };
