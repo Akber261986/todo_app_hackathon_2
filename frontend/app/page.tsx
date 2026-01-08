40a84ee49fc5cfc0cf8d7058449e141edc842f3e
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2026-01-08 16:18:42 +0500

    import error resolved

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 'use server';
 
-import { checkAuth } from '../../lib/auth-server';
+import { checkAuth } from '../lib/auth-server';
 import { redirect } from 'next/navigation';
 
 export default async function HomePage() {
